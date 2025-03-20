93c43e2f2d5961b01d349e39209bee4d87834d65
rohitchopra-git <126790168+rohitchopra-git@users.noreply.github.com>
2025-03-20 05:47:28 +0530

    Update runTests.groovy

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -1,11 +1,3 @@
-// def call(String pytestHtmlReport) {
-//     sh ". venv/bin/activate && pytest --html=${pytestHtmlReport} || true"
-// }
-def call(String PYTEST_HTML_REPORT) {
-    try {
-        sh ". venv/bin/activate && pytest --html=${PYTEST_HTML_REPORT}"
-    } catch (Exception e) {
-        currentBuild.result = 'SUCCESS'
-        echo "Tests failed but pipeline continues: ${e.message}"
-    }
+def call(String pytestHtmlReport) {
+    sh ". venv/bin/activate && pytest --html=${pytestHtmlReport} || true"
 }
